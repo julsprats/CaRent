6deac7e3076c19dd622af0a6f045f58ff5536ff5
Julia <152922186+julsprats@users.noreply.github.com>
2024-08-07 15:35:29 +0200

    Updated LocationManager

--- CaRent/Control/LocationManager.swift
+++ CaRent/Control/LocationManager.swift
@@ -1,8 +1,39 @@
-//
-//  LocationManager.swift
-//  CaRent
-//
-//  Created by Julia Prats on 2024-08-04.
-//
+import CoreLocation
 
-import Foundation
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
+    
+    @Published var userLocation: CLLocationCoordinate2D?
+    @Published var locationError: String?
+
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            locationError = "Location access denied. Please enable location services in Settings."
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            locationError = "Unknown location authorization status."
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            self.userLocation = location.coordinate
+            self.locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationError = "Failed to find user's location: \(error.localizedDescription)"
+    }
+}
+
